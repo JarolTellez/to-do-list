099464eedc7cacd20341e0055cd61fdd27eebae5
JarolTellez <jarol.tellez228188@potros.itson.edu.mx>
2024-09-17 18:41:48 -0800

    TareasDAO terminada

--- src/datos/TareaDAO.js
+++ src/datos/TareaDAO.js
@@ -1,8 +1,102 @@
-const conexionBD=require("../config/conexionBD");
+const conexionBD = require("../config/conexionBD");
 
-class TareaDAO{
+class TareaDAO {
+  static async agregarTarea(tarea) {
+    const connection = await conexionBD.conectar();
 
-    static async agregarTarea(){
-
+    try {
+      const tareaAgregada = await connecion.query(
+        "INSERT INTO tarea (nombre,descripcion,fechaCreacion,ultimaActualizacion,completada,idUsuario,prioridad) VALUES(?,?,?,?,?,?,?)"
+      );
+      tarea.id = tareaAgregada.insertertId;
+      return tarea;
+    } catch (error) {
+      console.log("Error al agregar una tarea: ", error);
+      throw error;
+    } finally {
+      connection.release();
     }
+  }
+
+  static async actualizarTarea(tarea) {
+    const connection = await conexionBD.conectar();
+
+    try {
+      await connecion.query(
+        "UPDATE tarea SET nombre = ?, descripcion = ?, fechaCreacion = ?, ultimaActualizacion = ?, completada = ?, idUsuario = ?, prioridad = ?",
+        [
+          tarea.nombre,
+          tarea.descripcion,
+          tarea.fechaCreacion,
+          fecha.fechaUltimaActualizacion,
+          fecha.completada,
+          fecha.idUsuario,
+          fecha.prioridad,
+        ]
+      );
+
+      return tarea;
+    } catch (error) {
+      console.log("Error al actualizar una tarea: ", error);
+      throw error;
+    } finally {
+      connection.release();
+    }
+  }
+
+  static async eliminarTarea(idTarea) {
+    const connection = await conexionBD.conectar();
+
+    try {
+      await connecion.query("DELETE FROM tarea WHERE idTarea = ?", [idTarea]);
+    } catch (error) {
+      console.log("Error al eliminar una tarea: ", error);
+      throw error;
+    } finally {
+      connection.release();
+    }
+  }
+
+  static async consultarTodasTareas() {
+    const connection = await conexionBD.conectar();
+
+    try {
+      const [tareas] = await connecion.query("SELECT * FROM tarea");
+      return tareas;
+    } catch (error) {
+      console.log("Error al consultar todas las tareas: ", error);
+      throw error;
+    } finally {
+      connection.release();
+    }
+  }
+
+  static async consultarTareaPorNombre(nombreTarea) {
+    const connection = await conexionBD.conectar();
+
+    try {
+      const [tarea] = await connecion.query("SELECT * FROM tarea WHERE nombre = ?",[nombreTarea]);
+      return tarea[0];
+    } catch (error) {
+      console.log("Error al consultar una tarea por nombre: ", error);
+      throw error;
+    } finally {
+      connection.release();
+    }
+  }
+
+  static async consultarTareaPorId(idTarea) {
+    const connection = await conexionBD.conectar();
+
+    try {
+      const [tarea] = await connecion.query("SELECT * FROM tarea WHERE idTarea = ?",[idTarea]);
+      return tarea[0];
+    } catch (error) {
+      console.log("Error al consultar una tarea por id: ", error);
+      throw error;
+    } finally {
+      connection.release();
+    }
+  }
+
 }
