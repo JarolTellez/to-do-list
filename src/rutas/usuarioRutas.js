32e14c90dbfe652bf8191c1f2c146c70626b1602
JarolTellez <jarol.tellez228188@potros.itson.edu.mx>
2024-09-19 09:22:34 -0800

    Cambio a uso de express, cambios solo en agregar usuario funcionando

---
+++ src/rutas/usuarioRutas.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const UsuarioController = require('../logica/UsuarioController');
+//const { handleErrors } = require('../utils/appError');
+const router = express.Router();
+
+
+// Definir las rutas
+router
+  .route('/')
+    .post(UsuarioController.agregarUsuario)
+    //.get(UsuarioController.); para obtener los usuarios pero aun no tengo el metodo
+
+// router.use(handleErrors);
+
+module.exports = router;
