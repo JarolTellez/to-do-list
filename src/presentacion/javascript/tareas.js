2b2bc58d50837b15d72867a0cc30741f1afae29b
JarolTellez <jarol.tellez228188@potros.itson.edu.mx>
2024-09-23 09:18:34 -0800

    Cambio de archivo js en la logica donde se abre el modal y creacion de archivo tareas.js para manejar en el cliente la logica para solicitar al server lo del crud de treas

---
+++ src/presentacion/javascript/tareas.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const tituloTarea=document.querySelector(".titulo_tarea");
+    const descripcion_tarea=this.document.querySelector(".descripcion_tarea");
+    const etiquetas=document.querySelector(".lista_Etiquetas");
+    const prioridad= document.querySelector('input[name="prioridad"]:checked');
+    const btnAgregarTarea=documebt.querySelector(".agregarModal");
+
+    btnAgregarTarea.addEventListener("click",function(){
+
+    });
+
+});
+
+          
+          
