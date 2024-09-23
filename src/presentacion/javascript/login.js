9ec3d6e2e5403ef935e77a23c78fa93fa050c159
JarolTellez <jarol.tellez228188@potros.itson.edu.mx>
2024-09-23 09:18:34 -0800

    Cambio de archivo js en la logica donde se abre el modal y creacion de archivo tareas.js para manejar en el cliente la logica para solicitar al server lo del crud de treas

--- src/presentacion/javascript/login.js
+++ src/presentacion/javascript/login.js
@@ -1,45 +1,51 @@
-document.addEventListener("DOMContentLoaded", function(){
-    const inputNombreUsuario = document.querySelector("#nombreUsuario");
-    const inputContrasenaUsuario = document.querySelector("#contrasenaUsuario");
-   const btnLogin=document.querySelector( "#btnLogin");
+document.addEventListener("DOMContentLoaded", function () {
+  const inputNombreUsuario = document.querySelector("#nombreUsuario");
+  const inputContrasenaUsuario = document.querySelector("#contrasenaUsuario");
+  const btnLogin = document.querySelector("#btnLogin");
+  const btnCerrarSesion = document.querySelector("#cerrarSesion");
 
-   btnLogin.addEventListener("click",function(){
+  btnLogin.addEventListener("click", function () {
     login();
-   });
+  });
 
-   async function login(){
-        const usuarioLogin = {
-            nombreUsuario: inputNombreUsuario.value,
-            contrasena: inputContrasenaUsuario.value
-        };
-        const urlUsuarioLogin="http://localhost:3000/usuario/login";
+  btnCerrarSesion.addEventListener("click", function () {
+    cerrarSesion();
+  });
 
-        try {
-         
-            const response = await fetch(urlUsuarioLogin, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(usuarioLogin) 
-            });
+  async function login() {
+    const usuarioLogin = {
+      nombreUsuario: inputNombreUsuario.value,
+      contrasena: inputContrasenaUsuario.value,
+    };
+    const urlUsuarioLogin = "http://localhost:3000/usuario/login";
 
-           
-            const data = await response.json();
-            
-            if (response.ok) {
-                console.log('Login exitoso:', data);
-                alert("Login exitoso!");
-                window.location.href = "principal.html";
-            } else {
-               
-                console.log('Error al iniciar sesion:', data.mensaje);
-                alert(data.mensaje);
-            }
-        } catch (error) {
-            console.error('Hubo un error con la solicitud de login:', error);
-            alert("Hubo un error con la solicitud de login.");
-        }
+    try {
+      const response = await fetch(urlUsuarioLogin, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(usuarioLogin),
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        console.log("Login exitoso:", data);
+        alert("Login exitoso!");
+        window.location.href = "principal.html";
+      } else {
+        console.log("Error al iniciar sesion:", data.mensaje);
+        alert(data.mensaje);
+      }
+    } catch (error) {
+      console.error("Hubo un error con la solicitud de login:", error);
+      alert("Hubo un error con la solicitud de login.");
     }
+  }
 
-    });
+  function cerrarSesion() {
+    console.log("Cerrando sesion ");
+    window.location.href = "index.html";
+  }
+});
