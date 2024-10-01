70eaeefa66573b4a4e69089ff88cdbdd2ed1ca5b
JarolTellez <jarol.tellez228188@potros.itson.edu.mx>
2024-09-30 17:18:02 -0800

    Funcionalidad para seleccionar etiquetas

--- src/presentacion/javascript/tareas.js
+++ src/presentacion/javascript/tareas.js
@@ -1,18 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
   const tituloTarea = document.querySelector(".tituloTarea");
   const descripcionTarea = document.querySelector(".descripcionTarea");
-  const inputEtiqueta = document.getElementById('contenedorInput');
-  const listaEtiquetas = document.getElementById('listaEtiquetas');
-  const contenedorConsultadas = document.getElementById('consultadas');
+  const inputEtiqueta = document.getElementById("contenedorInput");
+  const listaEtiquetas = document.getElementById("listaEtiquetas");
+  const contenedorConsultadas = document.getElementById("consultadas");
   const btnAgregarTarea = document.querySelector(".agregarModal");
   const etiquetas = []; // Para almacenar las etiquetas seleccionadas
+  const etiquetasSeleccionadas = [];
 
   btnAgregarTarea.addEventListener("click", function () {
     agregarTarea();
   });
 
   window.addEventListener("load", cargarEtiquetas);
-
 
   async function agregarTarea() {
     const prioridad = document.querySelector('input[name="prioridad"]:checked');
@@ -57,98 +57,133 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   }
 
+  // Función para cargar las etiquetas desde el servidor
+  async function cargarEtiquetas() {
+    const urlEtiquetas = "http://localhost:3000/tarea/etiquetas";
+    const idUsuario = { idUsuario: sessionStorage.getItem("idUsuario") };
+    console.log("ID USUARIO DESDE TAREAS: ", idUsuario);
 
-
-// Función para cargar las etiquetas desde el servidor
-async function cargarEtiquetas() {
-  const urlEtiquetas = "http://localhost:3000/tarea/etiquetas";
-  const idUsuario = { idUsuario: sessionStorage.getItem("idUsuario") };
-  console.log("ID USUARIO DESDE TAREAS: ", idUsuario);
-
-  try {
-    const response = await fetch(urlEtiquetas, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(idUsuario),
-    });
-
-    if (!response.ok) {
-      throw new Error("No se pudieron cargar las etiquetas");
-    }
-
-    const result = await response.json();
-    return result.data.map(etiqueta => etiqueta.nombre); // Devolver solo los nombres de las etiquetas
-  } catch (error) {
-    console.log(error.message);
-    alert("Error al consultar las etiquetas: ", error.message);
-    return [];
-  }
-}
-
-// Función para renderizar las etiquetas seleccionadas
-function renderTags() {
-  listaEtiquetas.innerHTML = ''; // Limpiar las etiquetas anteriores
-  etiquetas.forEach(tag => {
-    const li = document.createElement('li');
-    li.textContent = tag;
-    listaEtiquetas.appendChild(li);
-  });
-}
-
-// Función para mostrar las sugerencias
-async function showSuggestions(query) {
-  contenedorConsultadas.innerHTML = ''; // Limpiar sugerencias previas
-  if (query) {
-    const etiquetasConsultadas = await cargarEtiquetas(); // Cargar etiquetas dinámicamente
-    const filteredTags = etiquetasConsultadas.filter(etiqueta => etiqueta.toLowerCase().includes(query.toLowerCase()) && !etiquetas.includes(etiqueta));
-
-    if (filteredTags.length > 0) {
-      filteredTags.forEach(tag => {
-        const li = document.createElement('li');
-        li.textContent = tag;
-        li.addEventListener('click', () => {
-          addTag(tag);
-        });
-        contenedorConsultadas.appendChild(li);
+    try {
+      const response = await fetch(urlEtiquetas, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(idUsuario),
       });
 
-      contenedorConsultadas.classList.add('active'); // Mostrar sugerencias
-    } else {
-      contenedorConsultadas.classList.remove('active');
+      if (!response.ok) {
+        throw new Error("No se pudieron cargar las etiquetas");
+      }
+
+      const result = await response.json();
+
+      return result.data;
+
+      // return result.data.map((etiqueta) => etiqueta.nombre); // Devolver solo los nombres de las etiquetas
+    } catch (error) {
+      console.log(error.message);
+      alert("Error al consultar las etiquetas: ", error.message);
+      return [];
     }
-  } else {
-    contenedorConsultadas.classList.remove('active'); // Ocultar sugerencias si no hay texto
   }
-}
 
-// Función para agregar una etiqueta
-function addTag(tag) {
-  if (tag && !etiquetas.includes(tag)) {
-    etiquetas.push(tag);
-    renderTags();
+  // Función para renderizar las etiquetas seleccionadas
+  function renderizarEtiquetas() {
+    console.log("ETIQUETAS SELECCIONADAS:", etiquetasSeleccionadas);
+    listaEtiquetas.innerHTML = ""; // Limpiar las etiquetas anteriores
+    etiquetasSeleccionadas.forEach((etiqueta) => {
+      const li = document.createElement("li");
+      if (etiqueta.idEtiqueta) {
+        li.setAttribute("data-id", etiqueta.idEtiqueta);
+      }
+      li.textContent = etiqueta.nombre;
+      listaEtiquetas.appendChild(li);
+    });
   }
-  inputEtiqueta.value = ''; // Limpiar el input
-  showSuggestions(''); // Limpiar sugerencias
-}
 
-// Evento para capturar el texto que ingresa el usuario
-inputEtiqueta.addEventListener('input', (e) => {
-  const query = e.target.value.trim();
-  if (query) {
-    showSuggestions(query);
-  } else {
-    contenedorConsultadas.classList.remove('active'); // Ocultar sugerencias si no hay texto
+  // Función para mostrar las sugerencias
+  async function mostrarEtiquetasConsultadas(query) {
+    contenedorConsultadas.innerHTML = ""; // Limpiar sugerencias previas
+    if (query) {
+      const etiquetasConsultadas = await cargarEtiquetas(); // Cargar etiquetas dinámicamente
+      etiquetas.length = 0;
+      etiquetasConsultadas.forEach((el) => etiquetas.push(el));
+
+      //Filtra para mostrar las que coincidan y que no han sido agregadas previamente en el input
+      const etiquetasFiltradas = etiquetasConsultadas.filter(
+        (etiqueta) =>
+          etiqueta.nombre.toLowerCase().includes(query.toLowerCase()) &&
+          !etiquetasSeleccionadas.find((el) => el.nombre === etiqueta.nombre)
+      );
+
+      if (etiquetasFiltradas.length > 0) {
+        etiquetasFiltradas.forEach((etiqueta) => {
+          const li = document.createElement("li");
+          li.textContent = etiqueta.nombre;
+          li.setAttribute("data-id", etiqueta.idEtiqueta);
+          li.addEventListener("click", () => {
+            agregarEtiquetaInput(etiqueta);
+          });
+          contenedorConsultadas.appendChild(li);
+        });
+
+        contenedorConsultadas.classList.add("active"); // Mostrar sugerencias
+      } else {
+        contenedorConsultadas.classList.remove("active");
+      }
+    } else {
+      contenedorConsultadas.classList.remove("active"); // Ocultar sugerencias si no hay texto
+    }
   }
-});
 
-// Evento para manejar cuando el usuario presiona la barra espaciadora
-inputEtiqueta.addEventListener('keydown', (e) => {
-  if (e.key === ' ') {
-    const query = inputEtiqueta.value.trim();
-    addTag(query);
+  // Función para agregar una etiqueta
+  function agregarEtiquetaInput(etiqueta) {
+    etiquetasSeleccionadas.push(etiqueta);
+    renderizarEtiquetas();
+    inputEtiqueta.value = ""; // Limpiar el input
+    mostrarEtiquetasConsultadas(""); // Limpiar sugerencias
   }
-});
 
+  function buscarCoincidencias(etiqueta) {
+    const etiquetaRegistrada = etiquetas.find(
+      (el) => el.nombre.toLowerCase() === etiqueta.toLowerCase()
+    );
+
+    const seleccionada = etiquetasSeleccionadas.find(
+      (el) => el.nombre.toLowerCase() === etiqueta.toLowerCase()
+    );
+
+    if (seleccionada) {
+      return false;
+    } else if (typeof etiqueta === "object") {
+      return etiqueta ;
+    }
+
+   return etiquetaRegistrada ? etiquetaRegistrada : {nombre:etiqueta};
+    
+  }
+
+  // Evento para capturar el texto que ingresa el usuario
+  inputEtiqueta.addEventListener("input", (e) => {
+    const query = e.target.value.trim();
+    if (query) {
+      mostrarEtiquetasConsultadas(query);
+    } else {
+      contenedorConsultadas.classList.remove("active"); // Ocultar sugerencias si no hay texto
+    }
+  });
+
+  // Evento para manejar cuando el usuario presiona la barra espaciadora
+  inputEtiqueta.addEventListener("keydown", (e) => {
+    if (e.key === " " || e.key==="Enter") {
+      const query = inputEtiqueta.value.trim();
+      const etiquetaEnviada = buscarCoincidencias(query);
+      console.log("etiqueta:",etiquetaEnviada);
+      if (etiquetaEnviada) {
+        agregarEtiquetaInput(etiquetaEnviada);
+      }
+      inputEtiqueta.value = "";
+    }
+  });
 });
