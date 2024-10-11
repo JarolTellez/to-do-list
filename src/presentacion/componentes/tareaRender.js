7619bad9a609abc14a383a046b5c8429442f0dd7
JarolTellez <jarol.tellez228188@potros.itson.edu.mx>
2024-10-10 16:46:07 -0800

    Avance en el mostrar detalle tarea

--- src/presentacion/componentes/tareaRender.js
+++ src/presentacion/componentes/tareaRender.js
@@ -5,10 +5,12 @@ export const rendersTareas = {
       const tareaDiv = document.createElement("div");
       tareaDiv.className = "tarea";
       tareaDiv.innerHTML = `
-      <div class="principal">
+      <div class="principalTarea" id="${tareaElemento.idTarea}">
          <div class="contendorTarea" value="${tareaElemento.idTarea}">
           <h3>${tareaElemento.nombre}</h3>
-           <p class="fechaActualidada">${tareaElemento.fechaUltimaActualizacion}</p>
+           <p class="fechaActualidada">${
+             tareaElemento.fechaUltimaActualizacion
+           }</p>
            ${
              tareaElemento.descripcion
                ? `<p class="textoTarea">${tareaElemento.descripcion}</p>`
@@ -40,10 +42,10 @@ export const rendersTareas = {
   <input type="checkbox" id="completado-${
     tareaElemento.idTarea
   }" class="checkbox-completado"  value="${tareaElemento}"/>
-  <label for="completado-${tareaElemento.idTarea}" class="checkbox-label"></label>
+  <label for="completado-${
+    tareaElemento.idTarea
+  }" class="checkbox-label"></label>
 </div>
-
-           
           </div> 
           </div>
         `;
@@ -61,9 +63,65 @@ export const rendersTareas = {
     });
   },
 
-  eliminarRenderEspecifico(contenedor,componenteTarea){
-    if(componenteTarea){
+  eliminarRenderEspecifico(contenedor, componenteTarea) {
+    if (componenteTarea) {
       contenedor.removeChild(componenteTarea);
     }
-  }
+  },
+
+  mostrarModalDetalleTarea(modalDetalle, tarea) {
+    const inputTituloDetalle = modalDetalle.querySelector(
+      "#tituloTareaDetalle"
+    );
+    const fechaCreacion = modalDetalle.querySelector("#fechaCreacionDetalle");
+    const fechaUltimaActualizacion = modalDetalle.querySelector(
+      "#fechaUltimaActualizacionDetalle"
+    );
+    const descripcionDetalle = modalDetalle.querySelector(
+      "#descripcionDetalle"
+    );
+    const descripcionLabel = document.querySelector(
+      "label[for='descripcionDetalle']"
+    );
+    const contenedorPrioridad = modalDetalle.querySelector(
+      "#prioridadDetalleTarea"
+    );
+
+    modalDetalle.style.display = "flex";
+    inputTituloDetalle.value = tarea.nombre;
+    if (!tarea.descripcion || tarea.descripcion === "") {
+      descripcionDetalle.style.display = "none";
+      descripcionLabel.style.display = "none";
+    } else {
+      descripcionDetalle.style.display = "block";
+      descripcionLabel.style.display = "block";
+      descripcionDetalle.value = tarea.descripcion;
+    }
+    fechaCreacion.textContent = tarea.fechaCreacion;
+    fechaUltimaActualizacion.textContent = tarea.fechaUltimaActualizacion;
+
+    //Limpia el campo prioridad para mostrar el nuevo
+    contenedorPrioridad.innerHTML = "";
+    //Verifica que haya prioridad en la tarea para mostrarla
+    if (tarea.prioridad) {
+      const prioridadHTML = `
+    <div class="prioridad-container">
+      <span class="prioridad-text">Prioridad</span>
+      <div class="prioridad-barra" style="width: ${
+        tarea.prioridad * 20
+      }%; background: linear-gradient(to right, rgba(0, 128, 0, 0.3), rgba(0, 128, 0, 1));">
+        <span class="prioridad-numero">${tarea.prioridad}</span>
+      </div>
+    </div>
+  `;
+
+      contenedorPrioridad.innerHTML = prioridadHTML;
+    }
+  },
+
+  ocultarModal(modal) {
+    if (modal) {
+      modal.style.display = "none";
+    }
+  },
 };
