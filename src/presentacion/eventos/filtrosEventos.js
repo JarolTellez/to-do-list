5610f0b28535fe901a4c2b95f6fa1d1cb9e1d395
JarolTellez <jarol.tellez228188@potros.itson.edu.mx>
2024-10-26 14:56:20 -0800

    Mantener filtro al cargar tareas cuando se muestra una tarea si es que hay filtro seleccionado

--- src/presentacion/eventos/filtrosEventos.js
+++ src/presentacion/eventos/filtrosEventos.js
@@ -6,31 +6,128 @@ import { rendersTareas } from "../componentes/tareaRender.js";
 
 //Para guardar el boton para filtrar por tareas pendientes para poder usarla en la funcion
 //que exporto abajo "botonPendientesChecked" ;e establezco el valor en el "DOMContentLoaded" 
-let tareasPendientesButton;
+let tareasPendientesButton,tareasCompletadasButton;
+let prioridadMayorButton,prioridadMenorButton;
+let campoTareas;
+let contenedorFiltros;
+
+let radioPrioridadFiltro=null;
+let tareasRenderizadasActuales;
+let estadoPendientesButton=false;
+let estadoCompletadasButton=false;
 
 export  function botonPendientesChecked(seleccionado){
   tareasPendientesButton.checked=seleccionado && seleccionado==true?true:false;
+  if(prioridadMayorButton.checked){
+
+    prioridadMayor();
+  }else if(prioridadMenorButton.checked){
+    prioridadMenor();
+  }
+ 
 }
 
+function prioridadMayor(){
+    
+  //Verifico que el radio si este seleccionado
+  if(prioridadMayorButton.checked){
+   actualizarListas();
+    contenedorFiltros.classList.add("filtro","prioridadMayorFiltro");
+    contenedorFiltros.classList.remove("prioridadMenorFiltro");
+    const tareasOrdenadasPrioridadMayor = ordenarMayorMenor(tareasRenderizadasActuales);
+    rendersTareas.renderizarTareas(campoTareas,tareasOrdenadasPrioridadMayor, true);
+    }
+}
+
+function prioridadMenor(){
+  //Verifico que el radio si este seleccionado
+ if(prioridadMenorButton.checked){
+   actualizarListas();
+   contenedorFiltros.classList.remove("prioridadMayorFiltro");
+   contenedorFiltros.classList.add("filtro","prioridadMenorFiltro");
+   const tareasOrdenadasPrioridadMenor = ordenarMenorMayor(tareasRenderizadasActuales);
+   rendersTareas.renderizarTareas(campoTareas,tareasOrdenadasPrioridadMenor,true);
+   }
+}
+
+function actualizarListas(){
+  tareasRenderizadasActuales=tareasPendientesButton.checked?[...tareasPendientes]:[...tareasCompletadas];
+
+}
+
+function ordenarMayorMenor(tareas) {
+  //Utilizo sort para ordenar de mayor a menor con respecto a la prioridad, pero uso slice para crear una
+  //copia del arreglo original para no modificarlo porque necesito las tareas pendientes tal cual estan y asi solo
+  //modifico la copia que es el arreglo ordenado de mayor a menor por prioridad
+  const tareasPendientesPrioridadMayor = tareas.slice().sort((a, b) => {
+    // Si la primer priroridad es null o undefined, mover al final
+    if (a.prioridad === null || a.prioridad === undefined) return 1;
+    // Si la segunda prioridad es null o undefined, mover al final
+    if (b.prioridad === null || b.prioridad === undefined) return -1;
+
+    return b.prioridad - a.prioridad;
+  });
+
+  return tareasPendientesPrioridadMayor;
+}
+
+function ordenarMenorMayor(tareas) {
+  const tareasOrdenadasMenorMayor = tareas.slice().sort((a, b) => {
+    // Si la primer priroridad es null o undefined, mover al final
+    if (a.prioridad === null || a.prioridad === undefined) return 1;
+    // Si la segunda prioridad es null o undefined, mover al final
+    if (b.prioridad === null || b.prioridad === undefined) return -1;
+
+    return a.prioridad - b.prioridad;
+  });
+
+  return tareasOrdenadasMenorMayor;
+}
+
+//Deselecciona una opcion ya seleccionada de las 2 opciones de filtro de prioridad y vuelve a cargar y renderizar
+// las tareas dependiendo si esta seleccionado las pendientes o las completadas para mostrarlas sin el filtro
+function deseleccionarPrioridad() {
+  document
+    .querySelectorAll('.contenedorFiltroPrioridad input[type="radio"]')
+    .forEach((radio) => {
+      radio.addEventListener("click", function () {
+        // Si ya está seleccionado y es el mismo que el anterior
+        if (this === radioPrioridadFiltro) {
+
+          //actualizo las listas para establecer las tareas actuales segun la opcion seleccionada es decir
+          //si es pendientes o completadas
+          actualizarListas();
+          //Renderizo ya con las tareas actuales actualizadas y asi mostrar las tareas ya sea pendientes o completadas pero
+          //sin filtro, asi como se agregaron a la base de datos
+          rendersTareas.renderizarTareas(campoTareas,tareasRenderizadasActuales,true);
+          this.checked = false; // Deseleccionar
+          radioPrioridadFiltro = null; // Reiniciar selección
+        } else {
+          radioPrioridadFiltro = this; // Actualizar el radio seleccionado
+        }
+      });
+    });
+    contenedorFiltros.classList.remove("prioridadMenorFiltro");
+    contenedorFiltros.classList.remove("prioridadMayorFiltro");
+}
+
+
 document.addEventListener("DOMContentLoaded", async function () {
-  const campoTareas = document.querySelector("#listaTareas");
+ campoTareas = document.querySelector("#listaTareas");
 
   //FILTROS
-  const contenedorFiltros = document.querySelector(".filtros");
+ contenedorFiltros = document.querySelector(".filtros");
  
  tareasPendientesButton = document.querySelector(
     "#tareasPendientesFiltro"
   );
-  const tareasCompletadasButton = document.querySelector(
+ tareasCompletadasButton = document.querySelector(
     "#tareasCompletadasFiltro"
   );
-  const prioridadMayorButton = document.querySelector("#prioridadMayorFiltro");
-  const prioridadMenorButton = document.querySelector("#prioridadMenorFiltro");
+   prioridadMayorButton = document.querySelector("#prioridadMayorFiltro");
+   prioridadMenorButton = document.querySelector("#prioridadMenorFiltro");
 
-  let radioPrioridadFiltro=null;
-  let tareasRenderizadasActuales;
-  let estadoPendientesButton=false;
-  let estadoCompletadasButton=false;
+ 
 
   //Actualizo las tareas pendientes y completadas para cargarlas antes de usarlas
   await actualizarListaTareas();
@@ -44,15 +141,12 @@ document.addEventListener("DOMContentLoaded", async function () {
    }
    cargarModoBase();
 
-   function actualizarListas(){
-      tareasRenderizadasActuales=tareasPendientesButton.checked?[...tareasPendientes]:[...tareasCompletadas];
-  
-   }
-
-  
+ 
 
   tareasCompletadasButton.addEventListener("click", function () {
+    if(!estadoCompletadasButton){
     estadoCompletadasButton=true;
+    estadoPendientesButton=false;
     //Le agrego la clase filtro al contenedor de los filtros para indicar que se aplico un filtro
     contenedorFiltros.classList.add("filtro");
     // contenedorFiltros.classList.remove("prioridadMenorFiltro");
@@ -71,11 +165,13 @@ document.addEventListener("DOMContentLoaded", async function () {
     }
     rendersTareas.renderizarTareas(campoTareas, tareasCompletadas, true);
     tareasRenderizadasActuales=[...tareasCompletadas];
+  }
   });
 
   tareasPendientesButton.addEventListener("click", function () {
+    if(!estadoPendientesButton){
     estadoCompletadasButton=false;
-    estadoPendientesButton=!estadoPendientesButton;
+    estadoPendientesButton=true;
     //Le agrego la clase filtro al contenedor de los filtros para indicar que se aplico un filtro
     contenedorFiltros.classList.add("filtro");
     // contenedorFiltros.classList.remove("prioridadMenorFiltro");
@@ -92,6 +188,7 @@ document.addEventListener("DOMContentLoaded", async function () {
     }
     rendersTareas.renderizarTareas(campoTareas, tareasPendientes, true);
     tareasRenderizadasActuales=[...tareasPendientes];
+  }
   });
 
 
@@ -102,80 +199,13 @@ document.addEventListener("DOMContentLoaded", async function () {
     prioridadMayor();
   });
 
-  function prioridadMayor(){
-    
-    //Verifico que el radio si este seleccionado
-    if(prioridadMayorButton.checked){
-     actualizarListas();
-      contenedorFiltros.classList.add("filtro","prioridadMayorFiltro");
-      contenedorFiltros.classList.remove("prioridadMenorFiltro");
-      const tareasOrdenadasPrioridadMayor = ordenarMayorMenor(tareasRenderizadasActuales);
-      rendersTareas.renderizarTareas(campoTareas,tareasOrdenadasPrioridadMayor, true);
-      }
-  }
+ 
 
   prioridadMenorButton.addEventListener("click", function () {
     
   prioridadMenor();
   });
 
-  function prioridadMenor(){
-     //Verifico que el radio si este seleccionado
-    if(prioridadMenorButton.checked){
-      actualizarListas();
-      contenedorFiltros.classList.remove("prioridadMayorFiltro");
-      contenedorFiltros.classList.add("filtro","prioridadMenorFiltro");
-      const tareasOrdenadasPrioridadMenor = ordenarMenorMayor(tareasRenderizadasActuales);
-      rendersTareas.renderizarTareas(campoTareas,tareasOrdenadasPrioridadMenor,true);
-      }
-  }
-
-  function ordenarMayorMenor(tareas) {
-    //Utilizo sort para ordenar de mayor a menor con respecto a la prioridad, pero uso slice para crear una
-    //copia del arreglo original para no modificarlo porque necesito las tareas pendientes tal cual estan y asi solo
-    //modifico la copia que es el arreglo ordenado de mayor a menor por prioridad
-    const tareasPendientesPrioridadMayor = tareas.slice().sort((a, b) => {
-      // Si la primer priroridad es null o undefined, mover al final
-      if (a.prioridad === null || a.prioridad === undefined) return 1;
-      // Si la segunda prioridad es null o undefined, mover al final
-      if (b.prioridad === null || b.prioridad === undefined) return -1;
-
-      return b.prioridad - a.prioridad;
-    });
-
-    return tareasPendientesPrioridadMayor;
-  }
-
-  function ordenarMenorMayor(tareas) {
-    const tareasOrdenadasMenorMayor = tareas.slice().sort((a, b) => {
-      // Si la primer priroridad es null o undefined, mover al final
-      if (a.prioridad === null || a.prioridad === undefined) return 1;
-      // Si la segunda prioridad es null o undefined, mover al final
-      if (b.prioridad === null || b.prioridad === undefined) return -1;
-
-      return a.prioridad - b.prioridad;
-    });
-
-    return tareasOrdenadasMenorMayor;
-  }
-
-  function deseleccionarPrioridad() {
-    document
-      .querySelectorAll('.contenedorFiltroPrioridad input[type="radio"]')
-      .forEach((radio) => {
-        radio.addEventListener("click", function () {
-          // Si ya está seleccionado y es el mismo que el anterior
-          if (this === radioPrioridadFiltro) {
-            this.checked = false; // Deseleccionar
-            radioPrioridadFiltro = null; // Reiniciar selección
-          } else {
-            radioPrioridadFiltro = this; // Actualizar el radio seleccionado
-          }
-        });
-      });
-      contenedorFiltros.classList.remove("prioridadMenorFiltro");
-      contenedorFiltros.classList.remove("prioridadMayorFiltro");
-  }
 
 
 //Observer para ver cuando se modifica el componente listaTareas y asi si esta aplicado un filtro
