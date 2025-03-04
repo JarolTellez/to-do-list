7c7f3f6079b045c485b19f090d7fd84b1e2c0894
JarolTellez <jarol.tellez228188@potros.itson.edu.mx>
2025-03-04 06:53:34 -0800

    Validaciones en todo el dominio

--- Server/src/dominio/Etiqueta.js
+++ Server/src/dominio/Etiqueta.js
@@ -8,6 +8,22 @@ class Etiqueta {
     this.nombreEtiqueta = nombreEtiqueta;
     this.idUsuario = idUsuario;
   }
+
+  validar(){
+    const errores=[];
+
+    if(!this.nombreEtiqueta|| this.nombreEtiqueta.trim()===''){
+      errores.push({campo:'nombreEtiqueta',mensaje:'El nombre de la etiqueta es obligatorio'});
+    }
+
+    if(!this.idUsuario){
+      errores.push({campo:'idUsuario',mensaje:"Falta el id del usuario en la etiqueta"})
+    }
+
+    if(errores.length>0){
+      throw new Error(JSON.stringify(errores));
+    }
+  }
 }
 
 module.exports = Etiqueta;
--- Server/src/dominio/TareaEtiqueta.js
+++ Server/src/dominio/TareaEtiqueta.js
@@ -4,6 +4,22 @@ class TareaEtiqueta{
         this.idTarea = idTarea;
         this.idEtiqueta = idEtiqueta;
       }
+
+      validar(){
+        const errores=[];
+
+        if(!this.idTarea){
+          errores.push({ campo: 'idTarea', mensaje: 'La etiqueta debe asigarnse a una tarea (IdTarea)' });
+        }
+
+        if(!this.idEtiqueta){
+          errores.push({ campo: 'idEtiqueta', mensaje: 'Falta el id de la etiqueta' });
+        }
+
+        if (errores.length > 0) {
+          throw new Error(JSON.stringify(errores)); // Lanzar errores como JSON
+        }
+      }
 }
 
 module.exports= TareaEtiqueta;
