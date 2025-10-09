944dac8ae39a35c4f666c8ae1860bdb80bbf2e3a
JarolTellez <jarol.tellez228188@potros.itson.edu.mx>
2025-10-09 13:17:46 -0800

    refactor: Actualizar modelos de datos

---
+++ Cliente/src/models/session.js
@@ -0,0 +1,25 @@
+class Session {
+  constructor({
+    id,
+    userId,
+    userAgent,
+    ip,
+    createdAt,
+    expiresAt,
+    isActive,
+    isExpired,
+    timeUntilExpiration,
+    isCurrent
+  }) {
+    this.id = id;
+    this.userId = userId;
+    this.userAgent = userAgent;
+    this.ip = ip;
+    this.createdAt = new Date(createdAt);
+    this.expiresAt = new Date(expiresAt);
+    this.isActive = isActive;
+    this.isExpired = isExpired;
+    this.timeUntilExpiration = timeUntilExpiration;
+    this.isCurrent = isCurrent;
+  }
+}
